4bec79f8977f4145690d0b92e25c5649d29088ee
steven-solomon <solomon.steven.m@gmail.com>
2016-12-30 19:52:01 -0500

    Rename wod_blog to rest_day

--- spec/repositories/wod_repo_spec.rb
+++ spec/repositories/wod_repo_spec.rb
@@ -1,7 +1,7 @@
 describe WodRepo do
   it 'parses wod name' do
     stub_request(:any, 'http://www.crossfitsouthbrooklyn.com/').
-      to_return(body: File.new('./spec/test_data/wod_blog.html'), status: 200)
+      to_return(body: File.new('./spec/test_data/rest_day.html'), status: 200)
 
     wod_name = WodRepo.wod
     expect(wod_name).to eql("Rest Day")
